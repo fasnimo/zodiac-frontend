b00970351d54668560d2347fc73ac02821c24a20
fasnimo <fasnimo@gmail.com>
2020-05-19 20:05:57 -0500

    works

--- src/components/comment.js
+++ src/components/comment.js
@@ -6,7 +6,7 @@ class Comment {
         this.baseURL = `http://localhost:3000/zodiacs/${zodiac_id}/comments`;        
     }
     // called in Zodiac.js to Post fetch for loadComments.
-    addComments() {
+    addComments(ul) {
         const configObj = {
             method: "POST",
             headers: {
@@ -17,11 +17,50 @@ class Comment {
         }
         return fetch(`${this.baseURL}`, configObj)
          .then(res => res.json())
-         .then(json => {
-            this.loadComments(json)
+         .then(json => { // beta
+            this.makeComment(json, ul) 
         })
-        .then(setTimeout("location.reload(true)", 100))
+        //  .then(json => {
+        //     this.loadComments(json)
+        // })
+        // .then(setTimeout("location.reload(true)", 100))
         .catch(error => console.log("Error: " + error))       
+    }
+
+    makeComment = (data, ul) => { // beta
+        //  debugger
+        // const ul = document.querySelector('ul')//.parentElement.dataset.id
+        // debugger
+        const li = document.createElement('li')
+        // debugger
+            li.innerText = `${data.data.attributes.post}`
+            //debugger
+        const button = document.createElement('button')
+            button.setAttribute('class', 'remove')
+            button.setAttribute('data-comment-id', this.id)
+            button.innerText = 'Remove' 
+            button.addEventListener('click', function(e){
+                const url = `http://localhost:3000/zodiacs/${e.target.parentElement.parentElement.parentElement.dataset.id}/comments/${e.target.dataset.commentId}`
+                //  zodiac id: e.target.parentElement.parentElement.parentElement.dataset.id
+                //  comment id: e.target.dataset.commentId  
+                const reqObj = {
+                      method: "DELETE",
+                      headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",   
+                      }
+                    };
+                  return fetch(url, reqObj)
+                  .then(res => res.json())
+                  .then(info => e.target.parentElement.remove(info)) // selected li to remove
+                  .catch(error => console.log(error)) 
+            })   
+            // li.innerText = `${this.post}`
+        li.appendChild(button)
+        ul.appendChild(li)
+        // debugger
+       data.appendChild(ul)
+    //    debugger
     }
 
      // called by addComments to load and delete comments made
@@ -29,12 +68,14 @@ class Comment {
         const li = document.createElement('li')
         const button = document.createElement('button')
         
-        li.innerHTML = `${this.post}`
+        li.innerText = `${this.post}`
         button.setAttribute('class', 'remove')
         button.setAttribute('data-comment-id', this.id)
-        button.innerHTML = 'Remove'
+        button.innerText = 'Remove'
         button.addEventListener('click', function(e){
             const url = `http://localhost:3000/zodiacs/${e.target.parentElement.parentElement.parentElement.dataset.id}/comments/${e.target.dataset.commentId}`
+            //  zodiac id: e.target.parentElement.parentElement.parentElement.dataset.id
+            //  comment id: e.target.dataset.commentId  
             const reqObj = {
                   method: "DELETE",
                   headers: {
@@ -47,6 +88,7 @@ class Comment {
               .then(info => e.target.parentElement.remove(info)) // selected li to remove
               .catch(error => console.log(error)) 
         })
+        // list.appendChild(button) // beta
         li.appendChild(button)
         list.appendChild(li)
     }
--- src/components/zodiac.js
+++ src/components/zodiac.js
@@ -17,13 +17,13 @@ class Zodiac {
             const input = document.createElement('input')
                 input.id = zodiac.id
             const button = document.createElement('button')
-                button.innerHTML = 'Add Behavior Here'
+                button.innerText = 'Add Behavior Here'
                 button.onclick = function(){ 
                     let id = this.parentElement.dataset.id
                     let value = document.getElementById(id).value
 
                     let comment = new Comment(value, id, zodiac.id)
-                    comment.addComments()
+                    comment.addComments(ul)
                     console.log(this)
                     //^^/ CALLS ON: addComment fetch from Comment.Js
                     // To find value and id: console.log(document.getElementById(this.parentElement.dataset.id).value)
@@ -40,10 +40,11 @@ class Zodiac {
             this.main.append(div)
             
             console.log(zodiac);
-           // calls on loadComment in Comment.Js
+           // calls on loadComment in Comment.Js for existing comments
             zodiac.attributes.comments.forEach(comment => {
                 let newComment = new Comment(comment.post, comment.id, zodiac.id)
-                newComment.loadComments(ul)
+                // newComment.loadComments(ul)
+                newComment.loadComments(ul) // beta
             })
            })
 
